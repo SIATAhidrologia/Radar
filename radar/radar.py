a42cde40a957f854af13c1bc31f503d9a305b2a4
seospinale <seospina@gmail.com>
2020-09-11 11:15:46 -0500

    Cambios de sintaxis para ser comptaible con python3

--- radar/radar.py
+++ radar/radar.py
@@ -387,7 +387,7 @@ class radar_process:
 		'----------\n'\
 		'FD_plana=Fractal_Dimension_Plain(tam).\n'\
 				
-		if radar_f90.dxp<>0.0:
+		if radar_f90.dxp!=0.0:
 			return np.log(self.cant_elem)/np.log(radar_f90.dxp)
 		else:
 			print ('Error: radar.dxp=0.0')
@@ -474,7 +474,7 @@ class radar_process:
 		conv=np.copy(self.fractal)
 		strat[strat>0]=1; strat[np.isnan(strat)]=0
 		conv[np.isfinite(conv)]=0; conv[np.isnan(conv)]=1
-		if umbral<>None:
+		if umbral!=None:
 			conv[self.ref>umbral]=1
 		#conv=self.image.dilation(conv,kernel=kernel)
 		conv=nd.binary_fill_holes(conv); conv=conv*2
@@ -657,7 +657,7 @@ class draw_func:
 		'\n'\
 		'Retornos\n'\
 		'----------\n'\
-		'Plotea la figura y si ruta <> None, la guarda.\n'\
+		'Plotea la figura y si ruta != None, la guarda.\n'\
 		'\n'\
 		'Ejemplo\n'\
 		'----------\n'\
@@ -669,7 +669,7 @@ class draw_func:
 		X,Y=np.meshgrid(longs,lats)
 		Y=Y[::-1]
 		#Cambia el tamano de la figura si tiene que hacerlo
-		if figsize<>None:
+		if figsize!=None:
 			fig=pl.figure(figsize=figsize)		
 		#Genera el lugar de ploteo con el Basemap
 		m = Basemap(projection='merc',
@@ -693,7 +693,7 @@ class draw_func:
 			yoffset=0.1,
 			linewidth=0.1)
 		#Cambia zeros por nana
-		if mask_value<>None:
+		if mask_value!=None:
 			imageIn=np.ma.array(imageIn,mask=imageIn==mask_value)
 		#Genera el mapa
 		demX,demY=m(X,Y)
@@ -707,15 +707,15 @@ class draw_func:
 			x,y = self.draw_circle(m,-75.5276,6.1937,i,c='k',lw=0.5)
 			XY.append([x,y])
 		#Si hay coordenadas de algo las dibuja
-		if xy<>None:
+		if xy!=None:
 			xc,yc=m(xy[0],xy[1])
 			m.plot(xc,yc,color=xyColor,
 				#s=30,
 				linewidth=1,)
 				#edgecolor='black')
-		if texto<>None:
+		if texto!=None:
 			pl.annotate(texto, xy=(0.1, 0.9), xycoords='axes fraction', size=16)
-		if ruta<>None:
+		if ruta!=None:
 			pl.savefig(ruta,bbox_inches='tight')
 		pl.show()
 		#return XY
